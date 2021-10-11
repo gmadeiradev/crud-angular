7bbe8fd118f6c4baf0d842c0f83f29a1f0a47f1a
Madeira <gabrielsmadeira@fac.pe.senac.br>
2021-10-11 18:19:37 -0300

    Create courses module and use route with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // pathMatch: 'full' -> verifica todas as rotas possíveis para courses antes de redirecionar
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  // lazy loading
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
